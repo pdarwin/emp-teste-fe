76e347ddf0cd108e89e0443dbc94fce0fd12e4f3
JATR22 <tr.joaoandre@gmail.com>
2022-03-28 23:39:28 +0100

    Implementação 'Código' Editora

--- src/Componentes/Editoras/Editoras.js
+++ src/Componentes/Editoras/Editoras.js
@@ -1,5 +1,96 @@
 import Button from "@mui/material/Button";
 
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import { Container, Paper } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
 export function Editoras() {
-  return <Button>Editoras</Button>;
+  const paperStyle = { padding: "50px 20px", width: 600, margin: "20px auto" };
+  const [nome, setNome] = useState("");
+  const [morada, setMorada] = useState("");
+  const [getEditoras, setEditoras] = useState([]);
+  const classes = useStyles();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const editora = { nome, morada };
+    console.log(editora);
+    fetch("http://localhost:8080/addEditora", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(editora),
+    }).then(() => {
+      console.log("Nova Editora Adicionada");
+    });
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:8080/getEditoras")
+      .then((res) => res.json())
+      .then((result) => {
+        setEditoras(result);
+      });
+  }, []);
+  return (
+    <Container>
+      <Paper elevation={3} style={paperStyle}>
+        <h1 style={{ color: "blue" }}>
+          <u>Adicionar Editora</u>
+        </h1>
+
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField
+            id="outlined-basic"
+            label="Nome de Editora"
+            variant="outlined"
+            fullWidth
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Morada"
+            variant="outlined"
+            fullWidth
+            value={morada}
+            onChange={(e) => setMorada(e.target.value)}
+          />
+          <Button variant="contained" color="secondary" onClick={handleClick}>
+            Submeter
+          </Button>
+        </form>
+      </Paper>
+      <h1>Editoras</h1>
+
+      <Paper elevation={3} style={paperStyle}>
+        {getEditoras.map((editora) => (
+          <Paper
+            elevation={6}
+            style={{
+              margin: "10px",
+              padding: "15px",
+              textAlign: "center",
+              lineBreak: "normal",
+            }}
+            key={editora.id}
+          >
+            Id:{editora.id}
+            <br />
+            Nome:{editora.nome}
+            <br />
+            Morada:{editora.morada}
+          </Paper>
+        ))}
+      </Paper>
+    </Container>
+  );
 }
