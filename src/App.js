28797b868030351610464986460621278bde1b37
pdarwin <46503478+pdarwin@users.noreply.github.com>
2022-03-30 12:35:54 +0100

    act

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
-import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Loja } from "./Componentes/Loja/Loja";
-import Login from "./Componentes/Login";
+import { Login } from "./Componentes/Login";
 import { Editoras } from "./Componentes/Admin/Editoras";
 import { Autores } from "./Componentes/Admin/Autores";
 import NavBar from "./Componentes/NavBar";
 import { useState } from "react";
 import { createTheme } from "@mui/material";
+import { indigo, orange } from "@mui/material/colors";
+import { Registo } from "./Componentes/Registo";
 
 function App() {
   const [user, setUser] = useState();
 
   const myTheme = createTheme({
     palette: {
-      primary: {
-        light: "#757ce8",
-        main: "#3f50b5",
-        dark: "#002884",
-        contrastText: "#fff",
-      },
-      secondary: {
-        light: "#ce822c",
-        main: "#ce822c",
-        dark: "#ce822c",
-        contrastText: "#000",
-      },
+      primary: indigo,
+      secondary: orange,
     },
   });
 
@@ -35,13 +26,22 @@ function App() {
         <Routes>
           <Route path="/" element={<Loja theme={myTheme} />}></Route>
           <Route path="/contactos"></Route>
-          <Route path="/login" element={<Login theme={myTheme} />}></Route>
+          <Route
+            path="/login"
+            element={<Login theme={myTheme} user={user} setUser={setUser} />}
+          ></Route>
+          <Route
+            path="/registo"
+            element={<Registo theme={myTheme} user={user} setUser={setUser} />}
+          ></Route>
           <Route path="/livros" element={<Autores theme={myTheme} />}></Route>
           <Route path="/autores"></Route>
           <Route
             path="/editoras"
             element={<Editoras theme={myTheme} />}
           ></Route>
+          <Route path="/clientes"></Route>
+          <Route path="/staff"></Route>
         </Routes>
       </BrowserRouter>
       {/*      <Header />
