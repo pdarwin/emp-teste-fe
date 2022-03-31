621bd39325992192c798542632827cba96549b6d
pdarwin <46503478+pdarwin@users.noreply.github.com>
2022-03-31 18:27:07 +0100

    act

--- src/Componentes/Admin/Editoras.js
+++ src/Componentes/Admin/Editoras.js
@@ -1,67 +1,129 @@
+import { ThemeProvider } from "@emotion/react";
+import {
+  Alert,
+  Button,
+  FormControl,
+  Grid,
+  Modal,
+  TextField,
+  Typography,
+} from "@mui/material";
 import * as React from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import { ThemeProvider } from "@emotion/react";
+import { indigo } from "@mui/material/colors";
 
 const columns = [
-  { field: "nome", headerName: "Nome", width: 167 },
-  { field: "morada", headerName: "Morada", width: 527, editable: true },
-  { field: "ativo", headerName: "Estado", width: 221 },
+  { field: "nome", headerName: "Nome", width: 200 },
+  { field: "morada", headerName: "Morada", width: 200 },
   {
     field: "ativo",
     headerName: "Ativo",
     type: "boolean",
-    width: 10,
+    width: 5,
     editable: true,
   },
-  ,
 ];
 
-function Formulário() {
-  if (validar()) {
+export default function Editoras({ theme, user, API_URL }) {
+  const [editoras, setEditoras] = React.useState([]);
+  const [editora, setEditora] = React.useState({
+    nome: "",
+    morada: "",
+  });
+
+  //Gestão do modal
+  const [open, setOpen] = React.useState(false);
+  const [errLevel, setErrLevel] = React.useState("error");
+  const [err, setErr] = React.useState("");
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  React.useEffect(() => {
+    getEditoras();
+  }, []);
+
+  function getEditoras() {
+    fetch(API_URL + "/getEditoras", {
+      headers: { "Content-type": "application/json" },
+    })
+      .then((response) => {
+        console.log(response);
+        // Validar se o pedido foi feito com sucesso. Pedidos são feitos com sucesso normalmente quando o status é entre 200 e 299
+        if (response.status !== 200) {
+          throw new Error("Erro:" + response.status);
+        }
+
+        return response.json();
+      })
+      .then((parsedResponse) => {
+        setEditoras(parsedResponse);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }
+
+  function gravar() {
     fetch(API_URL + "/addEditora", {
       method: "POST",
       headers: {
         "Content-type": "application/json",
       },
       body: JSON.stringify({
-        nome: newUser.nome,
-        morada: newUser.morada,
+        nome: editora.nome,
+        morada: editora.morada,
         ativo: true,
       }),
     })
       .then((response) => {
         // Validar se o pedido foi feito com sucesso. Pedidos são feitos com sucesso normalmente quando o status é entre 200 e 299
         console.log(response);
-        if (response.status !== 200) {
-          throw new Error(response.status.toString);
-        }
+        /*           if (response.status !== 200) {
+            throw new Error(response.status.toString);
+          } */
 
         return response.json();
       })
       .then((parsedResponse) => {
-        setUser({
-          ...user,
-          nome: newUser.nome,
-          morada: newUser.morada,
-        });
-        setErr("Formulário bem sucedido.");
-        setErrLevel("success");
-        handleOpen();
+        if (parsedResponse.statusOk) {
+          setEditoras([...editoras, editora]);
+          setEditora({ ...editora, nome: "", morada: "" });
+          setErr("Registo bem sucedido");
+          setErrLevel("success");
+          handleOpen();
+        } else {
+          setErr(parsedResponse.msg);
+          handleOpen();
+        }
       })
       .catch((error) => {
         alert(error);
       });
-  
+  }
+
   return (
-    <form className="form">
-      <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
+      <Typography variant="h5" my={3} align="center" color="primary">
+        Gestão de editoras
+      </Typography>
+      <div style={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={editoras}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+        />
+      </div>
+      <form className="form">
         <div>
           <Modal
             open={open}
             onClose={handleClose}
-            onClick={() => {
-              if (errLevel === "success") navigate("/");
-            }}
+            /*               onClick={() => {
+                if (errLevel === "success") navigate("/");
+              }} */
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
           >
@@ -75,15 +137,15 @@ function Formulário() {
           sx={{ backgroundColor: indigo[100], p: 8 }}
         >
           <Grid item xs={12}>
-            <Typography variant="h5">Adicionar uma Editora</Typography>
+            <Typography variant="h5">Registo de editora</Typography>
           </Grid>
-          <Grid item xs={3}>
-            <FormControl>
+          <Grid item xs={12}>
+            <FormControl variant="outlined">
               <TextField
                 label="Nome"
-                value={newUser.nome}
+                value={editora.nome}
                 onChange={(e) => {
-                  setNewUser({ ...newUser, nome: e.target.value });
+                  setEditora({ ...editora, nome: e.target.value });
                 }}
                 style={{ backgroundColor: "white" }}
                 type="text"
@@ -91,47 +153,36 @@ function Formulário() {
               />
             </FormControl>
           </Grid>
-
-      </ThemeProvider>
-    </form>
-  )
-
-export default function Editoras({ theme, API_URL }) {
-  const [editoras, setEditoras] = React.useState([]);
-
-  React.useEffect(() => {
-    getEditoras();
-  }, []);
-
-  function getEditoras() {
-    console.log(API_URL + "/getEditoras");
-    fetch(API_URL + "/getEditoras", {
-      headers: { "Content-type": "application/json" },
-    })
-      .then((response) => {
-        console.log(response);
-        // Validar se o pedido foi feito com sucesso. Pedidos são feitos com sucesso normalmente quando o status é entre 200 e 299
-        if (response.status !== 200) {
-          throw new Error("There was an error finding editoras!");
-        }
-
-        return response.json();
-      })
-      .then((parsedResponse) => {
-        setEditoras(parsedResponse);
-      })
-      .catch((error) => {
-        alert(error);
-      });
-  }
-  return (
-    <div style={{ height: 400, width: "100%" }}>
-      <DataGrid
-        rows={editoras}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-      />
-    </div>
+          <Grid item xs={12}>
+            <FormControl variant="outlined">
+              <TextField
+                label="Morada"
+                value={editora.morada}
+                onChange={(e) => {
+                  setEditora({ ...editora, morada: e.target.value });
+                }}
+                style={{ backgroundColor: "white" }}
+                type="text"
+                required
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              type="button"
+              size="large"
+              variant="contained"
+              color="primary"
+              className="form__custom-button"
+              onClick={gravar}
+              sx={{ m: 1 }}
+            >
+              Gravar
+              <input type="Submit" hidden></input>
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </ThemeProvider>
   );
 }
